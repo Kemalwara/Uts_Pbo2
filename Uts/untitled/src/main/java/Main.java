bfb4c9665e909e985d3cfb4ac2b86cef14f0ab0e
Kemalwara <122217093+Kemalwara@users.noreply.github.com>
2023-01-08 22:40:49 +0800

    Add files via upload

---
+++ Uts/untitled/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import frame.KotaViewFrame;
+import helpers.Koneksi;
+
+public class Main {
+    public static void main(String[] args) {
+       //xx Koneksi.getConnection();
+        KotaViewFrame viewFrame = new KotaViewFrame();
+        viewFrame.setVisible(true);
+
+
+    }
+}
